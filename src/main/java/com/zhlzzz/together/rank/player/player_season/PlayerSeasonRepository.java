f63786087b877e2cbb32aa9113441965cd1f4b3c
Unknown <zengy361email@163.com>
2018-05-21 01:56:12 +0800

    修改获取pubg-api的配置

--- src/main/java/com/zhlzzz/together/rank/player/player_season/PlayerSeasonRepository.java
+++ src/main/java/com/zhlzzz/together/rank/player/player_season/PlayerSeasonRepository.java
@@ -6,7 +6,5 @@ import java.util.Optional;
 
 public interface PlayerSeasonRepository extends Repository<PlayerSeasonEntity, Long> {
 
-//    PlayerSeasonEntity save(PlayerSeasonEntity playerSeasonEntity);
     Optional<PlayerSeasonEntity> findByUserIdAndSeasonId(Long userId, String seasonId);
-//    List<PlayerSeasonEntity> save(Iterable<PlayerSeasonEntity> entities);
 }
