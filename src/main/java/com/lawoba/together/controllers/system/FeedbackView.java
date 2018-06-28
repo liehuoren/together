964be0841296f1b90ec6a65f393f7f435b355d3d
liehuoren <zhouhongliez@sina.com>
2018-06-28 20:59:00 +0800

    修改意见反馈

--- src/main/java/com/lawoba/together/controllers/system/FeedbackView.java
+++ src/main/java/com/lawoba/together/controllers/system/FeedbackView.java
@@ -1,10 +1,13 @@
 package com.lawoba.together.controllers.system;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.lawoba.together.system.feedback.FeedbackEntity;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.NonNull;
+
+import java.time.LocalDateTime;
 
 @ApiModel(description = "反馈意见")
 @JsonPropertyOrder({"id","userId","email","content"})
@@ -30,6 +33,10 @@ public class FeedbackView {
 
     @ApiModelProperty(value = "反馈内容", example = "反馈反馈反馈")
     public String getContent() { return feedbackEntity.getContent(); }
+
+    @ApiModelProperty(value = "创建时间", example = "2017-12-13 12:03:20")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+0800")
+    public LocalDateTime getCreateTime() { return feedbackEntity.getCreateTime(); }
 
     @ApiModelProperty(value = "是否处理", example = "true")
     public Boolean isFinished() { return feedbackEntity.isFinished(); }
